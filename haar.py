b64a6ebb6b55f66163fe69b345c2958a50b6d501
Ola <o.rosik.rosinska@gmail.com>
2015-05-13 18:23:59 +0200

    compression in Haar transform

--- haar.py
+++ haar.py
@@ -31,3 +31,31 @@ def image_from_coefficients_gray(data):
         for j in range(len(result[i])):
             result[i][j] = int(result[i][j])
     return result
+
+
+def compress(data):
+    for i in range(len(data)):
+        for j in range(len(data[i])):
+            data[i][j] = int(data[i][j])
+    return data
+
+
+def haar_compression(data):
+    r, g, b = calculate_coefficients(data)
+    r = compress(r)
+    g = compress(g)
+    b = compress(b)
+
+    result = image_from_coefficients(r, g, b)
+    return result
+
+
+def haar_compression_grayscale(data):
+    cA, (cH, cV, cD) = calculate_coefficients_gray(data)
+    cA = compress(cA)
+    cH = compress(cH)
+    cD = compress(cD)
+
+    coefficients = cA, (cH, cV, cD)
+    result = image_from_coefficients_gray(coefficients)
+    return result
